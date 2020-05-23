abc9c3ea4eb0a22692683a5611a40ab9ad2e1396
ghuertas629 <gh5577@alum.uncw.edu>
2020-05-23 13:32:08 -0400

    final code and report

--- queries.sql
+++ queries.sql
@@ -1,14 +1,25 @@
-select s.country_name, s.year, s.sex, s.age, s.suicide_number, s.population, s.suicides_per_100k, s.generation, c.capital_name, c.capital_latitude, c.capital_longitude, c.continent_name 
-from suicides s
-join capitals c
-on c.country_name = s.country_name
-where s.suicides_per_100k > 15
-order by s.suicides_per_100k DESC;
+SELECT *
+FROM suicides;
 
+SELECT *
+FROM capitals;
 
-select s.country_name, s.year, s.sex, s.age, s.suicide_number, s.population, s.suicides_per_100k, s.generation, c.capital_name, c.capital_latitude, c.capital_longitude, c.continent_name 
-from suicides s
-join capitals c
-on c.country_name = s.country_name
-where s.suicides_per_100k > 15 and s.generation = 'Boomers'
-order by s.suicides_per_100k DESC;
+SELECT s.country_name, c.capital_name, s.year, s.sex, s.age, s.suicide_number, s.population, s.suicides_per_100k, s.generation, c.capital_latitude, c.capital_longitude, c.continent_name 
+FROM suicides s
+JOIN capitals c
+ON c.country_name = s.country_name
+ORDER BY s.country_name;
+
+SELECT s.country_name, c.capital_name, s.year, s.sex, s.age, s.suicide_number, s.population, s.suicides_per_100k, s.generation, c.capital_latitude, c.capital_longitude, c.continent_name 
+FROM suicides s
+JOIN capitals c
+ON c.country_name = s.country_name
+WHERE s.suicides_per_100k > 15
+ORDER BY s.suicides_per_100k DESC;
+
+SELECT s.country_name, c.capital_name, s.year, s.sex, s.age, s.suicide_number, s.population, s.suicides_per_100k, s.generation, c.capital_latitude, c.capital_longitude, c.continent_name 
+FROM suicides s
+JOIN capitals c
+ON c.country_name = s.country_name
+WHERE s.suicides_per_100k > 15 AND s.generation = 'Boomers'
+ORDER BY s.suicides_per_100k DESC;
